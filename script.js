b4710e1a24efdae12182c53e5366e3ea010cbf0a
OGHENEMINE <66541923+OGHENEMINE@users.noreply.github.com>
2023-09-07 00:33:36 +0100

    first project commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const DAY = document.querySelector("#day");
+const TIME = document.querySelector("#time");
+
+// GET THE CURRENT DAT
+const updateDay = () => {
+  const currentDayNumber = new Date().getDay();
+  const daysOfTheWeek = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+  ];
+  const currentDay = daysOfTheWeek[currentDayNumber];
+  DAY.textContent = currentDay;
+};
+
+// GET THE CURRENT TIME
+
+const updateTime = () => {
+  const hrs = new Date().getHours();
+  const mins = new Date().getMinutes();
+  const secs = new Date().getSeconds();
+  let currentTime = "";
+
+  if (hrs < 12) {
+    currentTime = `${hrs} : ${mins} : ${secs} AM`;
+  } else {
+    currentTime = `${hrs} : ${mins} : ${secs} PM`;
+  }
+
+  TIME.textContent = currentTime;
+};
+
+// updateTime();
+setInterval(updateTime, 1000);
+setInterval(updateDay, 1000);
